68baeb0518a327137fe0b03810bddea5d398673c
Nicolas Dotto <nicolas.dotto@hotmail.com>
2017-07-13 18:41:31 +0100

    Automated commit at 2017-07-13 17:41:31 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/searchpage.js
@@ -0,0 +1,52 @@
+var lunrIndex = null;
+var lunrData  = null;
+// Download index data
+$.ajax({
+  url: "/search-0fba2154.json",
+  cache: true,
+  method: 'GET',
+  success: function(data) {
+    lunrData = data;
+    lunrIndex = lunr.Index.load(lunrData.index);
+  }
+});
+
+$(document).ready(function () {
+  var fallbacksearch = $('#fallback-search');
+  fallbacksearch.hide();
+
+  $('input#search').on('keyup', function () {
+    // Get query
+    var query = $(this).val();
+    // Search for it
+    var result = lunrIndex.search(query);
+    // Output it
+    var searchresults = $('.searchresults');
+    var resultcount = $('#result-count');
+    if (result.length === 0) {
+      // Hide results
+      searchresults.hide();
+      resultcount.hide();
+      if (query.length == 0) {
+        fallbacksearch.hide();
+      } else {
+        fallbacksearch.show()
+      }
+    } else {
+      // Show results
+      resultcount.html('results: ' + result.length);
+      searchresults.empty();
+      for (var item in result) {
+        // A result only gives us a reference to a document
+        var ref = result[item].ref;
+        // Using the reference get the document
+        var doc = lunrData.docs[ref];
+        // Get the section of the site
+        var group = " <span class='badge'>" + doc.group + '</span>';
+        var searchitem = '<a class="list-group-item" href="' + doc.url + '">' + doc.title + group + '</a>';
+        searchresults.append(searchitem);
+      }
+      searchresults.show();
+    }
+  });
+});
