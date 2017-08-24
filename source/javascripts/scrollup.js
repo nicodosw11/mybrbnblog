a6def130762ecd2c86f0a939d8629d7d4286ccf2
Nicolas Dotto <nicolas.dotto@hotmail.com>
2017-08-24 18:43:05 +0100

    add scrollup navigation to about pages

---
+++ source/javascripts/scrollup.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  // Hide Header on on scroll down
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var navbarHeight = $('header').outerHeight();
+
+  $(window).scroll(function(event){
+      didScroll = true;
+  });
+
+  setInterval(function() {
+      if (didScroll) {
+          hasScrolled();
+          didScroll = false;
+      }
+  }, 250);
+
+  function hasScrolled() {
+      var st = $(this).scrollTop();
+
+      var bannerHeight = $(".intro-video").height();
+      if ($(window).scrollTop() > (bannerHeight - 10)) {
+        $('.nav').removeClass('is-hidden');
+        $('body.about.about_index, body.fr.fr_about.fr_about_index').addClass('padding-top');
+      }
+
+      // Make sure they scroll more than delta
+      if(Math.abs(lastScrollTop - st) <= delta)
+          return;
+
+      // If they scrolled down and are past the navbar, add class .nav-up.
+      // This is necessary so you never see what is "behind" the navbar.
+      if (st > lastScrollTop && st > navbarHeight){
+          // Scroll Down
+          $('header').removeClass('nav-down').addClass('nav-up');
+      } else {
+          // Scroll Up
+          if(st + $(window).height() < $(document).height()) {
+              $('header').removeClass('nav-up').addClass('nav-down');
+          }
+      }
+
+      lastScrollTop = st;
+  }
+});
