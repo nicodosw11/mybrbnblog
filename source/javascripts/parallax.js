55a98086f630aec24482c13686061420c37cf0c0
Nicolas Dotto <nicolas.dotto@hotmail.com>
2017-08-25 19:11:32 +0100

    add about sections rough structure + footer

--- source/javascripts/parallax.js
+++ source/javascripts/parallax.js
@@ -2,11 +2,17 @@ $(document).ready(function() {
   if ($("#js-parallax-window").length) {
     parallax();
   }
+  if ($("#js-parallax-window-2").length) {
+    parallax2();
+  }
 });
 
 $(window).scroll(function(e) {
   if ($("#js-parallax-window").length) {
     parallax();
+  }
+  if ($("#js-parallax-window-2").length) {
+    parallax2();
   }
 });
 
@@ -14,6 +20,24 @@ function parallax(){
   if( $("#js-parallax-window").length > 0 ) {
     var plxBackground = $("#js-parallax-background");
     var plxWindow = $("#js-parallax-window");
+
+    var plxWindowTopToPageTop = $(plxWindow).offset().top;
+    var windowTopToPageTop = $(window).scrollTop();
+    var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
+
+    var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
+    var windowInnerHeight = window.innerHeight;
+    var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
+    var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
+    var plxSpeed = 0.35;
+
+    plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
+  }
+}
+function parallax2(){
+  if( $("#js-parallax-window-2").length > 0 ) {
+    var plxBackground = $("#js-parallax-background-2");
+    var plxWindow = $("#js-parallax-window-2");
 
     var plxWindowTopToPageTop = $(plxWindow).offset().top;
     var windowTopToPageTop = $(window).scrollTop();
