3d6d5857f9f80fabd3d432e2bae6a7e4e75e1b5c
Nicolas Dotto <nicolas.dotto@hotmail.com>
2017-08-30 15:13:54 +0100

    Automated commit at 2017-08-30 14:13:54 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/all.js
+++ javascripts/all.js
@@ -10330,6 +10330,23 @@ $(document).ready(function() {
 //     }
 // }
 ;
+// $(window).on('load', function() {
+// // $(document).ready(function () {
+//   // Animate loader off screen
+//   $('.se-pre-con').fadeOut('slow');
+// });
+
+
+$(window).on('load', function(){
+  setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+});
+function removeLoader(){
+    $( ".se-pre-con" ).fadeOut(500, function() {
+      // fadeOut complete. Remove the loading div
+      $( ".se-pre-con" ).remove(); //makes page more lightweight
+  });
+}
+;
 // window.onload = function () {
 //   'use strict';
 //   var modal = document.querySelector("#modal");
