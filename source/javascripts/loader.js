f414e8ac57c370dbb22066314840e2996df8f1bb
Nicolas Dotto <nicolas.dotto@hotmail.com>
2017-08-30 15:12:26 +0100

    add loader in layout for page transitions

---
+++ source/javascripts/loader.js
@@ -0,0 +1,16 @@
+// $(window).on('load', function() {
+// // $(document).ready(function () {
+//   // Animate loader off screen
+//   $('.se-pre-con').fadeOut('slow');
+// });
+
+
+$(window).on('load', function(){
+  setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+});
+function removeLoader(){
+    $( ".se-pre-con" ).fadeOut(500, function() {
+      // fadeOut complete. Remove the loading div
+      $( ".se-pre-con" ).remove(); //makes page more lightweight
+  });
+}
