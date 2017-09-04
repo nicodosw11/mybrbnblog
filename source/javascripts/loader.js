028cb37bb1c87d8d53602dfbed4e7ef627f25ce8
Nicolas Dotto <nicolas.dotto@hotmail.com>
2017-09-04 09:48:36 +0100

    resume cards and dynamic modal

--- source/javascripts/loader.js
+++ source/javascripts/loader.js
@@ -7,7 +7,20 @@
 
 $(window).on('load', function(){
   // setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
-  setTimeout(removeLoader, 1000); //wait for page load PLUS two seconds.
+  setTimeout(removeLoader, 1000);
+
+  // $('.intro-video').delay(1000).hide(28000);
+  // $('.intro-about').delay(1000).fadeIn(28000);
+  // $(".intro-video").remove();
+
+  setTimeout(function(){
+    document.getElementById('section-intro-video').style.display = 'none';
+    document.getElementById('section-intro-fs').style.display = 'block';
+    $(".intro-video").remove();
+  }, 14000); //14secs
+
+
+
 });
 function removeLoader(){
     $( ".se-pre-con" ).fadeOut(500, function() {
@@ -15,3 +28,9 @@ function removeLoader(){
       $( ".se-pre-con" ).remove(); //makes page more lightweight
   });
 }
+
+function preloader(){
+  document.getElementById("loading").style.display = "none";
+  document.getElementById("content").style.display = "block";
+}//preloader
+window.onload = preloader;
