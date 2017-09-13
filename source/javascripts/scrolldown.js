80fa4eb073e849f8be775afdc15c39721a28edd9
Nicolas Dotto <nicolas.dotto@hotmail.com>
2017-09-13 18:15:01 +0100

    change javascript for different navigation mobile desktop

--- source/javascripts/scrolldown.js
+++ source/javascripts/scrolldown.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $(window).scroll(function () {
 
     if ($(window).scrollTop() > (5)) {
-      $('.nav-scroll').removeClass('is-hidden');
+      $('.nav-scroll-down').removeClass('is-hidden');
       $('body.portfolio.portfolio_index, body.fr.fr_portfolio.fr_portfolio_index').addClass('padding-top');
       $('body.projects.projects_index, body.fr.fr_projects.fr_projects_index').addClass('padding-top');
       $('body.search.search_index, body.fr.fr_search.fr_search_index').addClass('padding-top');
